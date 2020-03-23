164083ed0c7136bb54a034796e8e910232841054
JulienTnl <47159826+JulienTnl@users.noreply.github.com>
2020-03-23 13:33:27 +0100

    Add files via upload

---
+++ Majuscules.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+    <input v-model="texte"  placeholder="Rentrer le texte"/>
+
+    {{texte|majusculesTexte}}
+
+    </div>
+</template>
+<script>
+export default {
+    name: 'Majuscules',
+    data: function() {
+        return {
+            texte: "text",
+        }
+    },
+ filters: {
+     majusculesTexte: function(texte) {
+        return texte.toUpperCase()
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
